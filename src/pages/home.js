c112c1526570d3a4cb2cfe60373a63771792ed86
jona62 <jonathanjamesm66@gmail.com>
2019-11-15 22:55:24 -0500

    Nav bar

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react'
+import Navigation from './navigation'
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <Navigation />
+            </div>
+        )
+    }
+}
+
+export default Home;
---
+++ src/pages/navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+const Navigation = () => {
+    return (
+        <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home"> Change For Change </Navbar.Brand>
+        </Navbar>
+    );
+}
+
+export default Navigation;
